2576f7e52d1ddb33be8081f7a5392090d972f1c5
Rudy Grays <Egormatveev228@inbox.ru>
2024-10-30 12:52:34 +0500

    fix

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -5,6 +5,7 @@ import authRoutes from "./routes/authRoutes";
 import * as dotenv from "dotenv";
 import { authMiddleware } from "./middleware/authMiddleware";
 import cors from "cors";
+import { prisma } from "./prisma/prisma.client";
 
 dotenv.config();
 
@@ -15,7 +16,16 @@ server.use(express.json());
 server.use("/auth", authRoutes);
 server.use("/users", authMiddleware, managerRoutes);
 server.use("/tasks", authMiddleware, taskRoutes);
-
+server.get("/health", async (req, res) => {
+  try {
+    await prisma.$connect();
+    res.status(200).json({ message: "Database connected successfully!" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Database connection failed", details: error });
+  }
+});
 server.listen(process.env.PORT || 4200, () => {
   console.log("Server running on port 4200");
 });
