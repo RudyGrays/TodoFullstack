dc149fdebbe9e2230380d994bc12581e130ea1d0
Rudy Grays <Egormatveev228@inbox.ru>
2024-10-31 04:25:04 +0500

    fix

--- TodoList/src/entities/User/model/services/logout/logout.ts
+++ TodoList/src/entities/User/model/services/logout/logout.ts
@@ -3,12 +3,10 @@ import { UsersActions } from "@/features/Users";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { UserActions } from "../../slice/UserSlice";
 import { TaskActions } from "@/entities/Task";
-import { notificationActions } from "@/entities/Notification";
 
 export const logout = createAsyncThunk("logout", (_, { dispatch }) => {
   dispatch(SubordinatesActions.initialState());
   dispatch(UsersActions.initialState());
   dispatch(UserActions.logout());
   dispatch(TaskActions.initialState());
-  dispatch(notificationActions.setNotification("Выход выполнен!"));
 });
